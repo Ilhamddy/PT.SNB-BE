24eddcba3ea363973461791659f371617884d664
ilhamddy <ilhammaulana9906@gmail.com>
2024-04-17 16:18:45 +0700

    fix: fix command message get data

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import express, { Request, Response,   static as static_ } from "express";
+import express, { Request, Response, static as static_ } from "express";
 import cors from "cors";
 import router from "./routes/RoleRoute";
-import { NewsRouter } from './routes/NewsRoute';
+import { NewsRouter } from "./routes/NewsRoute";
 import dotenv from "dotenv";
 import { UserRouter } from "./routes/UserRoute";
 import { ServicesRouter } from "./routes/ServicesRoute";
@@ -9,16 +9,13 @@ import { FaqRouter } from "./routes/FaqsRoute";
 
 import { join } from "path";
 
-
-
 dotenv.config();
 const app = express();
 
 const PORT = process.env.APP_PORT;
 app.use(express.json());
-app.use(cors())
-app.use('/', static_(join(__dirname, '../public')));
-
+app.use(cors());
+app.use("/", static_(join(__dirname, "../public")));
 
 app.get("/api", (req: Request, res: Response) => {
   return res.status(200).send({
@@ -27,24 +24,20 @@ app.get("/api", (req: Request, res: Response) => {
   // res.send("hello");
 });
 
-
 const newsRouter = new NewsRouter();
 const userRouter = new UserRouter();
 const faqRouter = new FaqRouter();
 const servicesRouter = new ServicesRouter();
 
-
 app.use(router);
 
-app.use('/api/news', newsRouter.getRouter());
-app.use('/api/user', userRouter.getRouter());
-app.use('/api/faq', faqRouter.getRouter());
-app.use('/api/services', servicesRouter.getRouter());
-
-
+app.use("/api/news", newsRouter.getRouter());
+app.use("/api/user", userRouter.getRouter());
+app.use("/api/faq", faqRouter.getRouter());
+app.use("/api/services", servicesRouter.getRouter());
 
 app.get("/", (req: Request, res: Response) => {
-  return res.send("Berhasil get Data");
+  return res.send("Request success: Data successfully retrieved from server");
 });
 
 app.listen(PORT, () => {
